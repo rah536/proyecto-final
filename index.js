976f73316df1f93d4e0279801c675dd75f4dad1f
Roberto Herrera <raherrera536@gmail.com>
2025-11-06 20:06:22 -0300

    Clase11_ruta productos + carpetas

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hola Express");
+    res.send("Bienvenido !!!");
 });
 
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
+
+//clase10
+let productos = [
+    {id: 1, categoria: "micha"},
+    {id: 2, categoria: "gala"},
+    {id: 3, categoria: "chiquito"},
+];
+
+app.get("/api/productos", (req, res) => {
+    res.json(productos);
+});
+//
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
